6bd2c2d5ca0866a438c9d821458c53d9bf3de780
Nuwan Saranath <saranathnuwan@gmail.com>
2023-07-30 18:02:44 +0530

    Complete Task 1

---
+++ DemoTask1.java
@@ -0,0 +1,7 @@
+public class DemoTask1{
+    public static void main(String[] args) {
+        double bankAccountBalance = 123456.7890;
+        System.out.printf("Your Balance is $%,.2f",bankAccountBalance);
+
+    }
+}

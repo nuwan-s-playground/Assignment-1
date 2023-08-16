1034879097702f6b1bd7572ae2556108a37cbc7f
nuwan saranath <saranathnuwan@gmail.com>
2023-08-16 08:39:31 +0530

    Initial commit

---
+++ DemoTask3.java
@@ -0,0 +1,10 @@
+public class DemoTask3 {
+    public static void main(String[] args) {
+        int numberOne = 123;
+        int numberTwo = 456;
+        int numberThree = 7890;
+        String redColor = "033[35m";
+        String resetColor = "033[0m";
+        System.out.printf("");
+    }
+}
